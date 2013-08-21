23e5082598ce858a29c48f4e2030717ae473009c
Atle Frenvik Sveen <atle.sveen@gmail.com>
2013-08-21 19:20:40 +0200

    add redis cache

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ import os
 import json
 import requests
 from flask import Flask, render_template, jsonify
+from werkzeug.contrib.cache import RedisCache
+import urlparse
+
+redis_url = urlparse.urlparse(os.environ.get('REDISTOGO_URL', 'redis://localhost:6379'))
 
 app = Flask(__name__)
+cache = RedisCache(host=redis_url.hostname, port=redis_url.port, password=redis_url.password, db=0, default_timeout=10 * 60 * 1000, key_prefix="beer_")
 
 @app.route('/')
 def index():
@@ -14,6 +19,10 @@ def index():
 
 @app.route('/api/<int:venue_id>')
 def api(venue_id):
-    url = "http://api.untappd.com/v4/venue/info/%s?client_id=%s&client_secret=%s" %(venue_id, os.environ['UT_CLIENT_ID'], os.environ['UT_CLIENT_SECRET'])
-    r = requests.get(url)
-    return jsonify(r.json())
+    data = cache.get(str(venue_id))
+    if data is None:
+        url = "http://api.untappd.com/v4/venue/info/%s?client_id=%s&client_secret=%s" %(venue_id, os.environ['UT_CLIENT_ID'], os.environ['UT_CLIENT_SECRET'])
+        r = requests.get(url)
+        data = r.json()
+        cache.set(str(venue_id), data)
+    return jsonify(data)
