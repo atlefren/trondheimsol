d6de3b6b48cb9908ae577976e4e38aa4947cc9e5
Atle Frenvik Sveen <atle.sveen@gmail.com>
2013-08-21 17:45:22 +0200

    mock api created

--- app.py
+++ app.py
@@ -2,10 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import os
+import json
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/<int:venue_id>')
+def api(venue_id):
+    print venue_id
+
+    filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data.json')
+    json_data = open(filename)
+    json.load(json_data)
